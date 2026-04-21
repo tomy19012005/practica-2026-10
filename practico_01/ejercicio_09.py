f20b1c84bd42e546ae2015dd66b015c904ee328b
juaquin11 <joaaperalta@gmail.com>
2026-04-21 18:27:59 -0300

    Terminado practico_01

--- practico_01/ejercicio_09.py
+++ practico_01/ejercicio_09.py
@@ -6,7 +6,10 @@ def sumatoria_basico(n: int) -> int:
 
     Restricción: Utilizar un bucle for.
     """
-    pass # Completar
+    suma = 0
+    for i in range(1, n + 1):
+        suma += i
+    return suma
 
 
 # NO MODIFICAR - INICIO
@@ -22,7 +25,7 @@ def sumatoria_sum(n: int) -> int:
     """Re-Escribir utilizando la función sum y sin usar bucles.
     Referencia: https://docs.python.org/3/library/functions.html#sum
     """
-    pass # Completar
+    return sum(range(1, n + 1))
 
 
 # NO MODIFICAR - INICIO
@@ -41,7 +44,7 @@ def sumatoria_reduce(n: int) -> int:
     """CHALLENGE OPCIONAL: Re-escribir utilizando reduce.
     Referencia: https://docs.python.org/3/library/functools.html#functools.reduce
     """
-    pass # Completar
+    return reduce(lambda acc, x: acc + x, range(1, n + 1), 0)
 
 
 # NO MODIFICAR - INICIO
@@ -58,7 +61,7 @@ def sumatoria_gauss(n: int) -> int:
     """CHALLENGE OPCIONAL: Re-Escribir utilizando suma de Gauss.
     Referencia: https://es.wikipedia.org/wiki/1_%2B_2_%2B_3_%2B_4_%2B_%E2%8B%AF
     """
-    pass # Completar
+    return (n * (n + 1)) // 2
 
 
 # NO MODIFICAR - INICIO
