82a866a294734aeb65f43e6a8b00cc42c7e45fea
juaquin11 <joaaperalta@gmail.com>
2026-05-26 16:20:53 -0300

    Restaurar archivos de pruebas de propiedades y desafíos originales

--- practico_01/practico_01_property_test.py
+++ practico_01/practico_01_property_test.py
@@ -1,46 +1,43 @@
-def test_asserts_ejercicio_01():
-    import ejercicio_01
+from hypothesis import given
+import hypothesis.strategies as st
 
 
-def test_asserts_ejercicio_02():
-    import ejercicio_02
+# Ejercicio 1
+
+from ejercicio_01 import maximo_basico, maximo_libreria, maximo_ternario
 
 
-def test_asserts_ejercicio_03():
-    import ejercicio_03
+@given(st.floats(allow_nan=False, allow_infinity=False), st.floats(allow_nan=False, allow_infinity=False))
+def test_ejercicio_01_commutative(x, y):
+    assert maximo_basico(x, y) == maximo_basico(y, x)
+    assert maximo_libreria(x, y) == maximo_libreria(y, x)
+    assert maximo_ternario(x, y) == maximo_ternario(y, x)
 
 
-def test_asserts_ejercicio_04():
-    import ejercicio_04
+@given(st.floats(allow_nan=False, allow_infinity=False), st.floats(allow_nan=False, allow_infinity=False))
+def test_ejercicio_01_idempotent(x, y):
+    assert maximo_basico(maximo_basico(x, y), maximo_basico(y, x)) == maximo_basico(y, x)
+    assert maximo_libreria(maximo_libreria(x, y), maximo_libreria(y, x)) == maximo_libreria(y, x)
+    assert maximo_ternario(maximo_ternario(x, y), maximo_ternario(y, x)) == maximo_ternario(y, x)
 
 
-def test_asserts_ejercicio_05():
-    import ejercicio_05
+@given(st.floats(allow_nan=False, allow_infinity=False, max_value=0, exclude_max=True),
+       st.floats(allow_nan=False, allow_infinity=False, min_value=0, exclude_min=True))
+def test_ejercicio_01_positives_negatives(x, y):
+    assert maximo_basico(x, y) == y
+    assert maximo_libreria(x, y) == y
+    assert maximo_ternario(x, y) == y
 
 
-def test_asserts_ejercicio_06():
-    import ejercicio_06
+@given(st.floats(allow_nan=False, allow_infinity=False, max_value=0, exclude_max=True), st.just(0))
+def test_ejercicio_01_negatives(x, y):
+    assert maximo_basico(x, y) == y
+    assert maximo_libreria(x, y) == y
+    assert maximo_ternario(x, y) == y
 
 
-def test_asserts_ejercicio_07():
-    import ejercicio_07
-
-
-def test_asserts_ejercicio_08():
-    import ejercicio_08
-
-
-def test_asserts_ejercicio_09():
-    import ejercicio_09
-
-
-def test_asserts_ejercicio_10():
-    import ejercicio_10
-
-
-def test_asserts_ejercicio_11():
-    import ejercicio_11
-
-
-def test_asserts_ejercicio_12():
-    import ejercicio_12
+@given(st.floats(allow_nan=False, allow_infinity=False, min_value=0, exclude_min=True), st.just(0))
+def test_ejercicio_01_positives(x, y):
+    assert maximo_basico(x, y) == x
+    assert maximo_libreria(x, y) == x
+    assert maximo_ternario(x, y) == x
